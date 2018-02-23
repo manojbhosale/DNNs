fdada0c0c87c75e867de1528107f8f7cc6f3a0af
Manojkumar <manojbhosale@users.noreply.github.com>
2018-02-23 17:38:57 +0530

    Update server.py

--- sampleCode/server.py
+++ sampleCode/server.py
@@ -13,8 +13,16 @@ def apicall():
 	
 	try:
 		test_json = request.get_json()
-		test = pd.read_json(test_json, orient = 'records')
 		
+		#input ='[{"Loan_ID":"LP001015","Gender":1,"Married":1,"Dependents":"0","Education":0,"Self_Employed":0,"ApplicantIncome":5720,"CoapplicantIncome":0,"LoanAmount":110,"Loan_Amount_Term":360,"Credit_History":1,"Property_Area":1}]'
+		
+		#input = '[{"Loan_ID":"LP001015","Gender":"Male","Married":"Yes","Dependents":"0","Education":"Graduate","Self_Employed":"No","ApplicantIncome":"5720","CoapplicantIncome":0,"LoanAmount":"110","Loan_Amount_Term":"360","Credit_History":"1","Property_Area":"Urban"}]'
+		
+		print(test_json,type(test_json))
+		
+		#print(type(jsono), jsono)
+		#test = pd.read_json(test_json, orient = 'records')
+		test = pd.DataFrame(test_json)
 		test['Dependents'] = [str(x) for x in list(test['Dependents'])]
 		
 		loan_ids = test['Loan_ID']
@@ -30,13 +38,16 @@ def apicall():
 		print("Loading the model ...")
 		loaded_model = None
 		
-		with open('./model/'+clf, 'rb') as f:
+		with open('./models/'+clf, 'rb') as f:
 			loaded_model = pickle.load(f)
 		print("The model has been loaded...doing predictions now...")
+		testdf = pd.DataFrame(test); #need for succssful run in transformation step
+		print(testdf)
+		#a = test['Dependents']
+		print("Type --- ",type(testdf))
+		predictions = loaded_model.predict(testdf)
 		
-		predictions = loaded_model.predict(test)
-		
-		prediciton_series = list(pd.Series(predictions))
+		prediction_series = list(pd.Series(predictions))
 		
 		final_predictions = pd.DataFrame(list(zip(loan_ids, prediction_series)))
 		responses = jsonify(predictions = final_predictions.to_json(orient="records"))
@@ -46,12 +57,5 @@ def apicall():
 		return (responses)
 		
 		
-		
-		
-		
-		
-		
-		
-
-		
-	
+if __name__ == '__main__':
+	app.run()
